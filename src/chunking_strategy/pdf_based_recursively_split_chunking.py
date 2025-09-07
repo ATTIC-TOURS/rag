36c33299ae44d7f7397f6db4a7a218a1e756eb2b
KrimssmirK <krimssmirk003@gmail.com>
2025-09-07 23:41:09 +0800

    modify chunking

--- src/chunking_strategy/pdf_based_recursively_split_chunking.py
+++ src/chunking_strategy/pdf_based_recursively_split_chunking.py
@@ -3,16 +3,20 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 class PdfBasedRecursivelySplitChunking(ChunkingStrategy):
-    strategy_name = "pdf_based_recursively_split_chunking"
+    strategy_name = "pdf_based_recursively_split_chunking_v2"
 
-    def __init__(self, pdf_num_split: int, chunk_overlap_rate: float):
+    def __init__(self, chunk_overlap_rate: float, max_tokens: int):
         super().__init__()
-        self.pdf_num_split = pdf_num_split
         self.chunk_overlap_rate = chunk_overlap_rate
+        self.max_tokens = max_tokens
 
     def chunk(self, text: str) -> list[str]:
         text_len = len(text)
-        chunk_size = text_len // self.pdf_num_split
+        tokens = text.split()
+        
+        pdf_num_split = (len(tokens) // self.max_tokens) + 1
+        
+        chunk_size = text_len // pdf_num_split
         chunk_overlap = int(chunk_size * self.chunk_overlap_rate)  
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=chunk_size, 
