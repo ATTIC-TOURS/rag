4288007691b4025132e19518e144baa38709ecf8
KrimssmirK <krimssmirk003@gmail.com>
2025-09-21 12:55:56 +0800

    add retriever

--- rag_pipeline/retriever/retriever.py
+++ rag_pipeline/retriever/retriever.py
@@ -6,11 +6,24 @@
 from llama_index.vector_stores.weaviate import WeaviateVectorStore
 from llama_index.core import VectorStoreIndex, StorageContext
 import weaviate
+from sentence_transformers import CrossEncoder
+import time
 
 
-def retrieve_query_relevant_docs(text: str, similarity_top_k: int, index_name: str):
+connection_config = {"port": 8080, "grpc_port": 50051, "skip_init_checks": True}
+
+def retrieve_query_relevant_docs(
+    text: str, 
+    similarity_top_k: int, 
+    alpha: float, 
+    index_name: str, 
+    cross_encoder_model: str | None = None,
+    rerank_top_k: int | None = None
+):
+    start = time.time()  # record start time
+    
     client = None
-    connection_config = {"port": 8080, "grpc_port": 50051, "skip_init_checks": True}
+    
     try:
         client = weaviate.connect_to_local(**connection_config)
         vector_store = WeaviateVectorStore(
@@ -20,7 +33,39 @@ def retrieve_query_relevant_docs(text: str, similarity_top_k: int, index_name: s
 
         index = VectorStoreIndex([], storage_context=storage_context)
         
-        return index.as_retriever().retrieve(text) 
+        retriever = index.as_retriever(
+                similarity_top_k=similarity_top_k,
+                mode="hybrid",
+                alpha=alpha
+            )
+        
+        docs = retriever.retrieve(text)
+
+        if cross_encoder_model:
+            cross_encoder = CrossEncoder(cross_encoder_model)
+
+            # prepare pairs: (query, doc_text)
+            pairs = [(text, d.text) for d in docs]
+
+            relevance_scores = cross_encoder.predict(pairs)
+
+            reranked = sorted(
+                zip(docs, relevance_scores),
+                key=lambda x: x[1],
+                reverse=True
+            )
+
+            # keep top rerank_top_k (or all if None)
+            if rerank_top_k:
+                reranked = reranked[:rerank_top_k]
+
+            docs = [d for d, _ in reranked]
+        end = time.time()  # record end time
+    
+        elapsed = end - start
+        print(f"Elapsed time: {elapsed:.2f} seconds")
+        
+        return docs
 
     except Exception as e:
         print("error in retrieve_query_relevant_docs")
