54d95b018d9168c4c963f4799c34269e7ecf8b24
KrimssmirK <krimssmirk003@gmail.com>
2025-09-22 17:01:39 +0800

    apply ragas

--- experiment.py
+++ experiment.py
@@ -1,28 +1,34 @@
 import os
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
 import time
 import pandas as pd
+
 from langchain_openai import ChatOpenAI
-from ragas.metrics import (
-    faithfulness,
-    answer_relevancy,
-    context_precision,
-    context_recall,
-)
-from ragas.evaluation import evaluate
 from ragas.llms import LangchainLLMWrapper
-from ragas.embeddings import LangchainEmbeddingsWrapper
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.chat_models import ChatOllama
+from ragas import evaluate
+from ragas.metrics import (
+    Faithfulness,
+    FactualCorrectness,
+    LLMContextPrecisionWithReference,
+    LLMContextRecall,
+)
 from ragas.run_config import RunConfig
-from datasets import Dataset
+from ragas import EvaluationDataset
+
+from langchain_community.chat_models import ChatOllama
 
 from rag_pipeline.rag_pipeline import build_rag_pipeline
 from llama_index.core.prompts import PromptTemplate
 from dotenv import load_dotenv
-from colorama import init, Fore
-init(autoreset=True)
 
 load_dotenv()
+from colorama import init, Fore
+
+init(autoreset=True)
+import warnings
+
+warnings.filterwarnings("ignore", category=ResourceWarning)
 
 RESULTS_FILE = "rag_experiment_results.csv"
 
@@ -40,30 +46,30 @@ def transform_queries(df: pd.DataFrame) -> pd.DataFrame:
     """
     Clean and keep only relevant columns:
     - query_id
-    - query (renamed to 'question')
-    - answer (renamed to 'ground_truth')
+    - query (renamed to 'user_input')
+    - answer (renamed to 'reference')
     - optional: filter rows based on `check` column
     """
     df = df.copy()
 
     # Rename columns to match RAGAS expected format
-    df = df.rename(columns={"query": "question", "answer": "ground_truth"})
+    df = df.rename(columns={"query": "user_input", "answer": "reference"})
 
     # Drop rows with missing data
-    df = df.dropna(subset=["question", "ground_truth"])
+    df = df.dropna(subset=["user_input", "reference"])
 
     # Optional: filter only rows where check == 1 (if you use that column for QA validation)
     # if "check" in df.columns:
     #     df = df[df["check"] == 1]
 
-    return df[["query_id", "question", "ground_truth"]]
+    return df[["query_id", "user_input", "reference"]]
 
 
 def load_queries() -> pd.DataFrame:
-    print(Fore.CYAN + 'Loading Query and Answer pairs from Google Spreadsheet..')
+    print(Fore.CYAN + "Loading Query and Answer pairs from Google Spreadsheet..")
     df = extract_queries()
     df = transform_queries(df)
-    print(Fore.CYAN + f'💿 Total Rows: {len(df)} retrieved')
+    print(Fore.CYAN + f"💿 Total Rows: {len(df)} retrieved")
     return df
 
 
@@ -73,64 +79,58 @@ EVAL_DF = load_queries()
 EVAL_QUERIES = EVAL_DF.to_dict(orient="records")
 
 
-def run_experiment(params, openai_api_key):
+def run_experiment(params):
     """
     Run one experiment with given params, return averaged metrics + latency.
     """
-    
+
     query_engine, client = build_rag_pipeline(**params)
 
     results = []
     latencies = []
 
     # Generate response for each query
-    print(Fore.BLUE + f'🤖 generating response..')
-    for q in EVAL_QUERIES:
+    len_eval_queries = len(EVAL_QUERIES)
+    for idx, q in enumerate(EVAL_QUERIES):
+        print(Fore.BLUE + f"{idx+1}/{len_eval_queries} 🤖 generating response..")
         start = time.time()
-        response = query_engine.query(q["question"])
+        response = query_engine.query(q["user_input"])
         elapsed = time.time() - start
         latencies.append(elapsed)
 
         results.append(
             {
-                "question": q["question"],
-                "answer": str(response),
-                "contexts": [n.get_content() for n in response.source_nodes],
-                "ground_truth": q["ground_truth"],
+                "user_input": q["user_input"],
+                "response": str(response),
+                "retrieved_contexts": [n.get_content() for n in response.source_nodes],
+                "reference": q["reference"],
             }
         )
-    print(Fore.BLUE + f'response has been generated')
 
     client.close()
 
-    dataset = Dataset.from_list(results)
+    dataset = EvaluationDataset.from_list(results)
 
-    # Define LLM for evaluation
-    # llm = ChatOpenAI(
-    #     model="gpt-4o-mini",
-    #     api_key=openai_api_key,
-    #     temperature=0
-    # )
-
-    eval_llm = ChatOllama(model="gemma3:1b", temperature=0.0)
-    ragas_llm = LangchainLLMWrapper(eval_llm)
-
-    eval_embeddings = HuggingFaceEmbeddings(model_name="intfloat/multilingual-e5-base")
-    ragas_embeddings = LangchainEmbeddingsWrapper(eval_embeddings)
+    # llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.0)
+    llm = ChatOllama(model="gemma3:1b", temperature=0.0)
+    evaluator_llm = LangchainLLMWrapper(llm)
 
     run_config = RunConfig(timeout=7200)
 
-    print(Fore.BLUE + 'evaluating..')
     eval_results = evaluate(
-        dataset,
-        metrics=[faithfulness, answer_relevancy, context_precision, context_recall],
-        llm=ragas_llm,
-        embeddings=ragas_embeddings,
+        dataset=dataset,
+        metrics=[
+            Faithfulness(),
+            FactualCorrectness(),
+            LLMContextPrecisionWithReference(),
+            LLMContextRecall(),
+        ],
+        llm=evaluator_llm,
         run_config=run_config,
     )
 
     # for retriever and generator
-    avg_metrics = eval_results.to_pandas().describe().loc["mean"].to_dict()
+    avg_metrics = eval_results.to_pandas().describe().loc["mean"].to_dict()  # type: ignore
 
     # latency
     avg_metrics["avg_latency_sec"] = sum(latencies) / len(latencies)
@@ -142,8 +142,8 @@ def run_experiment(params, openai_api_key):
 def log_results(params, metrics):
     record = {**params, **metrics}
 
-    del record['openai_api_key']
-    
+    del record["openai_api_key"]
+
     if os.path.exists(RESULTS_FILE):
         df = pd.read_csv(RESULTS_FILE)
         df = pd.concat([df, pd.DataFrame([record])], ignore_index=True)
@@ -151,13 +151,15 @@ def log_results(params, metrics):
         df = pd.DataFrame([record])
 
     df.to_csv(RESULTS_FILE, index=False)
-    print(Fore.BLUE + f"📃 Logged experiment: {record}")
+    print(Fore.BLUE + f"📝 Logged Experiment")
+
 
 def count_experiment_variables(vars):
     count = 1
     for var in vars:
         count *= len(var)
     return count
+
 
 def run_all_experiments(openai_api_key):
     """
@@ -175,42 +177,48 @@ def run_all_experiments(openai_api_key):
             "Answer clearly and concisely."
         ),
     ]
-    
-    num_variables = count_experiment_variables([
-        similarity_top_k_values,
-        alpha_values,
-        rerank_options,
-        prompt_templates
-    ])
-    
-    print(Fore.GREEN + f'Total Experiment: {num_variables}')
+
+    num_variables = count_experiment_variables(
+        [similarity_top_k_values, alpha_values, rerank_options, prompt_templates]
+    )
+
+    print(Fore.GREEN + f"Total Experiment: {num_variables}")
 
     current_ongoing_experiment = 1
     for top_k in similarity_top_k_values:
         for alpha in alpha_values:
             for reranker in rerank_options:
                 for prompt_template in prompt_templates:
+                    current_vars = {
+                        "similarity_top_k": top_k,
+                        "alpha": alpha,
+                        "cross_encoder_model": reranker,
+                        "prompt_template": prompt_template,
+                    }
                     params = {
+                        **current_vars,
                         "index_name": "Requirements",
                         "embeddings_model_name": "intfloat/multilingual-e5-base",
-                        "llm_model_name": "gemma3:1b",  
-                        "similarity_top_k": top_k,  
-                        "alpha": alpha,  
-                        "prompt_template": prompt_template, 
-                        "cross_encoder_model": reranker,
-                        "rerank_top_n": 1 if reranker else None, 
-                        "llm_provider": "ollama",  
-                        "openai_api_key": openai_api_key,  
+                        "llm_model_name": "gemma3:1b",
+                        "rerank_top_n": 1 if reranker else None,
+                        "llm_provider": "ollama",
+                        "openai_api_key": openai_api_key,
                     }
 
                     try:
-                        print(Fore.GREEN + f'🟢 {current_ongoing_experiment}/{num_variables} running experiment on params: {params}')
-                        metrics = run_experiment(params, openai_api_key)
+                        print(
+                            Fore.GREEN
+                            + f"🟢 {current_ongoing_experiment}/{num_variables} running experiment on: {current_vars}"
+                        )
+                        metrics = run_experiment(params)
                         log_results(params, metrics)
                     except Exception as e:
                         print(f"❌ Failed experiment {params}: {e}")
-                        
-    print(Fore.GREEN + f"✅ {num_variables} experiments done")
+
+    print(
+        Fore.GREEN + f"✅ {current_ongoing_experiment}/{num_variables} experiments done"
+    )
+
 
 if __name__ == "__main__":
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
